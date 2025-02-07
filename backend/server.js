4569c8bcadd7f41e07576d7b738ebfd0cab93ee7
Daniel Alvarez <daniel.yllanes@hotmail.com>
2025-02-07 11:06:40 -0500

    add if statement for node_env === production in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,14 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
+
+if (process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/dist'))
+    app.get('*', (req,res)=>{
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+    })
+}
 
 const PORT = process.env.PORT || 4141
 
